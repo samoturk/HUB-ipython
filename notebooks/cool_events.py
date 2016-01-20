5180cbf3906d9852bc6b16ac3c57a1b0b95e9aad
Samo Turk <samo.turk@gmail.com>
2016-01-20 23:03:15 +0100

    Update

--- notebooks/cool_events.py
+++ notebooks/cool_events.py
@@ -5,11 +5,12 @@ class HUB:
     """
     HUB event class
     """
-    def __init__(self):
+    def __init__(self, version):
         self.full_name = "Heidelberg Unseminars in Bioinformatics"
         self.info = HTML("<p>Heidelberg Unseminars in Bioinformatics are participant-"
             "driven meetings where people with an interest in bioinformatics " 
             "come together to discuss hot topics and exchange ideas and then go "
             "for a drink and a snack afterwards.</p>")
-    def __str__(self):
+        self.version = version
+    def __repr__(self):
         return self.full_name
